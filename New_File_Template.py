807d55998726cae39c70c2a390c6dd3540be0b87
Steve Walker <swwalker74@gamil.com>
2020-09-10 15:55:30 -0400

    Template to use to start new program files

---
+++ New_File_Template.py
@@ -0,0 +1,25 @@
+#! python3
+'''
+Written by Steve Walker
+Original release date: dd mmm yy
+Version: 0
+Release date: dd mmm yy
+'''
+
+''' List of MODULES for import if needed '''
+#import pyinputplus as pyip
+#import random			random number generation
+#import sys				system level functions sys.exit() for example
+#import os				operating system
+#import math			advanced math functions
+#import openpyxl		used to interact with MS Excel
+#import send2trash
+#import requests
+#import beautifulsoup4
+#import selenium
+#import PyPDF2
+#import python-docx
+#import imapclient
+#import twilio
+#import pillow
+#import pyautogui
